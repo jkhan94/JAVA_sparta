39e5b13f21e81aa8692e7884f16b757d2ec4dd4e
jkhan94 <hjkwork@naver.com>
2024-04-27 00:18:39 +0900

    week3 인터페이스

---
+++ src/week3/interfaceTest/Main.java
@@ -0,0 +1,44 @@
+package week3.interfaceTest;
+
+public class Main extends D implements C {
+
+    // 인터페이스 C의 추상메소드 구현
+    @Override
+    public void a() {
+        System.out.println("A");
+    }
+    @Override
+    public void b() {
+        System.out.println("B");
+    }
+
+    // 부모클래스 함수 오버라이딩
+    @Override
+    void d() {
+        super.d();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main(); // Main클래스 내의 멤버 메소드를 쓰기 위해 변수 main이란 Main 객체 생성.
+        main.a();
+        main.b();
+        main.d();
+    }
+}
+
+interface A {
+    void a();
+}
+
+interface B {
+    void b();
+}
+
+interface C extends A, B {
+}
+
+class D {
+    void d() {
+        System.out.println("D");
+    }
+}
